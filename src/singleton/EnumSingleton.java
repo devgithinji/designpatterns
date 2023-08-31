3f6f709c63169a7c92ef59997fe5f915299180dc
dennis githinji <wakahiad@gmail.com>
2023-08-31 13:29:51 +0300

    fix using Enums

--- src/singleton/EnumSingleton.java
+++ src/singleton/EnumSingleton.java
@@ -1,5 +1,9 @@
 package singleton;
 
+//advantages
+//Enums constructor gets invoked by the JVM not by the user who is using
+//we don't need to worry about multithreading as enums are thread safe
+//it solves the issue of serialization since JVM takes care of serialization
 public enum EnumSingleton {
     INSTANCE;
     public void doSomething() {
