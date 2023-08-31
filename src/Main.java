bc46992220b576991450ff32a6a5e90745590a0e
dennis githinji <wakahiad@gmail.com>
2023-08-31 12:36:50 +0300

    singleton principle breaking using objectstream serialization

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 import singleton.LazySingleton;
 
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+//        LazySingleton lazySingleton = LazySingleton.getInstance();
+//        System.out.println(lazySingleton.getName("dennis"));
+        exampleSerialization();
+    }
+
+    private static void exampleSerialization() throws IOException, ClassNotFoundException {
         LazySingleton lazySingleton = LazySingleton.getInstance();
-        System.out.println(lazySingleton.getName("dennis"));
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("object.obj"));
+        objectOutputStream.writeObject(lazySingleton);
+        objectOutputStream.close();
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("object.obj"));
+        LazySingleton deserializedLazy = (LazySingleton) objectInputStream.readObject();
+        objectInputStream.close();
+
+        System.out.println("Object 1 :" + lazySingleton.hashCode());
+        System.out.println("Object 2 :" + deserializedLazy.hashCode());
     }
 }
