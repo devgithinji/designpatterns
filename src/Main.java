cc4f49d1aed95a46572222659c4ee008eed14748
dennis githinji <wakahiad@gmail.com>
2023-08-31 12:53:33 +0300

    resolving breaking of singleton pattern via using a serilaizable singleton class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import singleton.LazySingleton;
+import singleton.SerializableSingleton;
 
 import java.io.*;
 
@@ -10,16 +10,29 @@ public class Main {
     }
 
     private static void exampleSerialization() throws IOException, ClassNotFoundException {
-        LazySingleton lazySingleton = LazySingleton.getInstance();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("object.obj"));
-        objectOutputStream.writeObject(lazySingleton);
+//        LazySingleton lazySingleton = LazySingleton.getInstance();
+//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("object.obj"));
+//        objectOutputStream.writeObject(lazySingleton);
+//        objectOutputStream.close();
+//
+//        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("object.obj"));
+//        LazySingleton deserializedLazy = (LazySingleton) objectInputStream.readObject();
+//        objectInputStream.close();
+//
+//        System.out.println("Object 1 :" + lazySingleton.hashCode());
+//        System.out.println("Object 2 :" + deserializedLazy.hashCode());
+
+
+        SerializableSingleton serializableSingleton = SerializableSingleton.getInstance();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("object1.obj"));
+        objectOutputStream.writeObject(serializableSingleton);
         objectOutputStream.close();
 
-        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("object.obj"));
-        LazySingleton deserializedLazy = (LazySingleton) objectInputStream.readObject();
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("object1.obj"));
+        SerializableSingleton deserializedInstance = (SerializableSingleton) objectInputStream.readObject();
         objectInputStream.close();
 
-        System.out.println("Object 1 :" + lazySingleton.hashCode());
-        System.out.println("Object 2 :" + deserializedLazy.hashCode());
+        System.out.println("SerilaizableSingleton Object 1 :" + serializableSingleton.hashCode());
+        System.out.println("SerilaizableSingleton Object 2 :" + deserializedInstance.hashCode());
     }
 }
